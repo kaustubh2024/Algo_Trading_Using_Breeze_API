7dcb1fa1d82bf3ee11f1825026fd4581bc4ef84e
kaustubh2024 <152265444+kaustubh2024@users.noreply.github.com>
2024-04-29 21:35:03 +0530

    Add files via upload

--- StopLossOrder.py
+++ StopLossOrder.py
@@ -1,103 +1,292 @@
 import login as l
+import json
 
-#intialize keys
-
-#api_key = "INSERT_YOUR _APP_KEY_HERE"
-#api_secret = "INSERT_YOUR_SECRET_KEY_HERE"
-#api_session = 'INSERT_YOUR_API_SESSION_HERE'
-
-
+from datetime import datetime
+from time import time, sleep
+import sys
 
 # Define Contract
-#stock = 'IDEA',
-#strike = '17750',
-#expiry = '2023-04-20T06:00:00.000Z',      
-#right = 'call',
-stock = 'IDEA'
-strike = '13'
+stock = 'IDECEL'
+order_id = ""
+price = 11.15
+cost = 0
+trailing_stoploss = 0
+order_id = ""
+qty = 12500
+# Create an empty list to store holding dictionaries
+holding_dicts = []
 
 
 # Import Libraries
-from datetime import datetime
-from time import time, sleep
+from datetime import time
+from time import sleep
 from breeze_connect import BreezeConnect
+
+# Define the target time
+target_time = time(15, 30)  # 3:30 PM
+print("\nTarget time is: " + str(target_time) + ".\n")
+
 
 # Setup my API keys 
 api = BreezeConnect(api_key=l.api_key)
 api.generate_session(api_secret=l.api_secret,session_token=l.session_key)
 
-api.set_funds(transaction_type="credit", amount="30", segment="Equity")
-print("\n************\n")
-print("Updated balance is: ")
-print(api.get_funds())
-print("\n************\n")
+def add_funds(amt):
+    api.set_funds(transaction_type="credit", amount=amt, segment="Equity")
+    print("\n************\n")
+    print("Updated balance is: ")
+    print(api.get_funds())
+    print("\n************\n")
+
+def buy_stock_in_cash(stock_code, qty):
+    global order_id
+    # Place order
+    buy_order = api.place_order(stock_code=stock_code,
+                                exchange_code="NSE",
+                                product="cash",
+                                action='buy',
+                                order_type='limit',
+                                stoploss="",
+                                quantity=qty,
+                                price="12.95",
+                                validity="day"
+                                )
+    print("\n*** Printing buy_order ***\n")
+    print(buy_order)
+
+    # Store Order ID
+    order_id = buy_order['Success']['order_id']
+    print("\n*****\n")
+
+    print("Order ID is: " + str(order_id) )
+
+    print("\n****** Order Placed ******\n")
+
+    print("\n*** Order Details ***\n")
+    print(api.get_order_detail(exchange_code="NSE",
+                            order_id=order_id))
+    print("\n*****\n")
+
+    print("\n*** Trade Details ***\n")
+    print(api.get_trade_detail(exchange_code="NSE",
+                            order_id=order_id))
+    print("\n*****\n")
 
 
+    print(stock, " - *** \n")
 
-# Place order
-buy_order = api.place_order(stock_code=stock,
-                            exchange_code="NSE",
-                            product="cash",
-                            action='buy',
-                            order_type='market',
-                            stoploss="",
-                            quantity="2",
-                            price="",
-                            validity="day",
-                            #validity_date=today,
-                            disclosed_quantity="0",
-                            #expiry_date=expiry,
-                            #right=right,
-                            #strike_price=strike
-                            )
+# Get current cost
+def get_cost(order_id):
+    global trailing_stoploss
+    global price
+    global trailing_stoploss
+    # Get the buy price of order
+    try:
+        cost = float(api.get_order_detail('NSE',order_id)['Success'][0]['average_price'])
+        trailing_stoploss = round((cost - 0.05),2)
+        price = cost
+    except:
+        print("\nException while getting the cost price. Cost price set to 0.0\n")
+        cost = 0
+        trailing_stoploss = cost
 
-# Store Order ID
-order_id = buy_order['Success']['order_id']
+    print("\nCost is: " + str(cost))
+    return cost
 
-# Get current price
-price = api.get_quotes(stock_code=stock,
-                exchange_code="NFO",
-                product_type="options",
-                #expiry_date=expiry,
-                #right=right,
-                strike_price=strike
-                )
+def get_my_portfolio_holdings():
+    portfolio = api.get_portfolio_holdings("NSE", "", "", stock, "")
+    print(portfolio)
 
-# Get the buy price of order
-cost = float(api.get_order_detail('nfo',order_id)['Success'][0]['average_price'])
-trailing_stoploss = round(cost * 0.95,1)
+    # Create an empty list to store holding dictionaries
+    global holding_dicts
 
-# Run Loop
-while(True):
+    # Check if portfolio is not empty and contains 'Success' key
+    if 'Success' in portfolio:
+        holding_list = portfolio['Success']
+        if holding_list:
+            print("\n*** Portfolio Begins ***\n")
+            # Iterate over each holding in the portfolio
+            for holding in holding_list:
+                # Create a new dictionary for each holding
+                holding_dict = {}
+                # Populate the dictionary with details of the holding
+                holding_dict['stock_code'] = holding['stock_code']
+                holding_dict['quantity'] = holding['quantity']
+                holding_dict['average_price'] = holding['average_price']
+                # Add more details as needed
+                # Append the holding dictionary to the list
+                holding_dicts.append(holding_dict)
+        else:
+            print("\nPortfolio is empty.\n")
+    else:
+        print("\nFailed to fetch portfolio holdings.\n")
 
-    if(price > cost * 1.10):
-        cost = price
-        trailing_stoploss = round(cost * 0.95,1)
+    # Print the list of holding dictionaries
+    for holding_dict in holding_dicts:
+        print(holding_dict)
+    print("\n*** Portfolio End ***\n")
 
-    elif(price < cost):
+    
+    # Check if portfolio is not empty and contains 'Success' key
+    if 'Success' in portfolio:
+        holding_list = portfolio['Success']
+        if holding_list:
+            print("\nPortfolio Holdings:\n")
+            # Iterate over each holding in the portfolio
+            for holding in holding_list:
+                print("\n")
+                print(type(holding))
+                print("\n")
+                # Print details of each holding
+                print("Stock Code:", holding['stock_code'])
+                print("Quantity:", holding['quantity'])
+                print("Average Price:", holding['average_price'])
+                # Add more details as needed
 
-        # Place square off order 
-        sq_off_order = api.square_off(exchange_code="NSE",
-                    product="cash",
-                    stock_code=stock,
-                    #expiry_date=expiry,
-                    #right=right,
-                    strike_price=strike,
-                    action='sell',
-                    order_type="market",
-                    validity="day",
-                    stoploss="",
-                    quantity="2",
-                    price="0",
-                    validity_date=datetime.now().strftime('%Y-%m-%dT06:00:00.000Z')  ,
-                    trade_password="",
-                    disclosed_quantity="0")
+                print("\n")  # Add a newline for better readability between holdings
+        else:
+            print("\nPortfolio is empty.\n")
+    else:
+        print("\nFailed to fetch portfolio holdings.\n")
+
+    
 
 
-        if(sq_off_order['Status']==200) : 
-            print(sq_off_order)
+if __name__ == '__main__':
+
+    #Connect to websocket
+    api.ws_connect()
+
+    # Get portfolio holdings
+    print("\nGetting portfolio holdings\n")
+    get_my_portfolio_holdings()
+    
+
+    add_funds(10)
+
+    # Stock = IDECEL, QTY = 2
+    buy_stock_in_cash(stock, qty)
+
+    #quote = api.get_quotes(stock_code=stock,
+    #            exchange_code="NSE",
+    #            product_type="cash"
+    #            )
+    #print(quote)
+    #price = quote['Success'][0]['ltp']
+    #price = quote['Success']['ltp']
+    #print("\nCurrent price is: " + str(price))
+
+    print("\n*** Extracting Cost and setting Trailing Stop-loss ***\n")
+    cost = float(get_cost(order_id))
+    #trailing_stoploss = round((cost - 0.10),2)
+    #trailing_stoploss = cost
+    print("\nCurrent trailing_stoploss is: " + str(trailing_stoploss))
+
+    # Callback to receive ticks
+    def on_ticks(ticks):
+        global price
+        #print("Ticks: {}".format(ticks['last']))
+        #price = quote['Success'][0]['ltp']
+        price = ticks.get("last")
 
 
-        break
+    def monitor_stoploss_and_square_off():
 
-    sleep(2)
+        global trailing_stoploss
+        # Subscribe stocks feeds
+        api.subscribe_feeds(exchange_code="NSE", 
+                                stock_code="IDECEL", 
+                                product_type="cash", 
+                                get_exchange_quotes=True, 
+                                get_market_depth=False
+                                )
+        
+        api.on_ticks = on_ticks
+        print("\n*** Monitoring live price quotes for stop loss *** \n")
+        print ("\n**********\n")
+        print ("Price is: " + str(price) + " , Cost is: " + str(cost) + " and trailing stoploss is: " + str(trailing_stoploss) )
+        print ("\n**********\n")
+        if cost == 0:
+            print("\n Cost is 0. It means BUY order was not executed. Exiting...\n")
+            # Terminate the program with exit code 1 (indicating an error)
+            sys.exit(1)
+
+        #IF Condition 0
+        if (price > cost * 1.05):
+            # Place square off order 
+            print ("Price is: " + str(price) + " , Cost is: " + str(cost) + " and trailing stoploss is: " + str(trailing_stoploss) )
+            
+            print("\n*** Squaring off at IF condition 0 *** at price: " + str(price) + ".\n")
+            # Place sell order
+            
+            sell_order = api.place_order(stock_code=stock,
+                        exchange_code="NSE",
+                        product="cash",
+                        action='sell',
+                        order_type='market',
+                        stoploss="",
+                        quantity=qty,
+                        price="",
+                        validity="day"
+                        )
+            
+        if(price < trailing_stoploss - 0.05):
+            # Place square off order 
+            print ("Price is: " + str(price) + " , Cost is: " + str(cost) + " and trailing stoploss is: " + str(trailing_stoploss) )
+            
+            print("\n*** Squaring off at IF condition 1 *** at price: " + str(price) + ".\n")
+            # Place sell order
+            
+            sell_order = api.place_order(stock_code=stock,
+                        exchange_code="NSE",
+                        product="cash",
+                        action='sell',
+                        order_type='market',
+                        stoploss="",
+                        quantity=qty,
+                        price="",
+                        validity="day"
+                        )
+            
+            # Terminate the program with exit code 0
+            sys.exit(0)
+
+        if(price < (cost - 0.05)):
+            # Place square off order 
+            print ("Price is: " + str(price) + " , Cost is: " + str(cost) + " and trailing stoploss is: " + str(trailing_stoploss))
+            
+            print("\n*** Squaring off at IF condition 2 *** at price: " + str(price) + ".\n")
+
+            # Place sell order
+            
+            sell_order = api.place_order(stock_code=stock,
+                        exchange_code="NSE",
+                        product="cash",
+                        action='sell',
+                        order_type='market',
+                        stoploss="",
+                        quantity=qty,
+                        price="",
+                        validity="day"
+                        )
+            
+            # Terminate the program with exit code 0
+            sys.exit(0)
+        
+        elif (price > trailing_stoploss + 0.10):
+            trailing_stoploss = round((price - 0.05), 2)
+            print("\n*** Revised trailing stoploss to: " + str(trailing_stoploss))
+
+        else:
+            print("\nPrice = " + str(price) + " and trailing_stoploss = " + str(trailing_stoploss) + ".\n")
+
+        sleep(2)
+
+    
+    #while(True):
+    while datetime.now().time() < target_time:
+        monitor_stoploss_and_square_off()
+        sleep(2)
+
+
+    print("\n\n\n*** It's now 3:30 PM. Exiting the trading desk ***\n\n\n")
